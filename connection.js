ff43fe0760a791bcef30aab2d6e98b19485d9639
Sean O’Sullivan <sosullivan425@gmail.com>
2026-07-17 14:24:09 -0400

    test

--- connection.js
+++ connection.js
@@ -1,15 +1,17 @@
 
 
-const button = document.getElementById("hostButton");
+const hostButton = document.getElementById("hostButton");
+const joinButton = document.getElementById("joinButton");
 
 function gotoRoomHost(){
     window.location.href = 'room.html'; // Path relative to index.html
 }
 
-function joinPeer(conn){
+function joinPeer(){
     var id = document.getElementById('peer-id-enter').innerText;
     window.location.href = "room.html?id="+id; // Path relative to index.html
 }
 
-button.addEventListener("click", gotoRoomHost);
+hostButton.addEventListener("click", gotoRoomHost);
+joinB.addEventListener("click", joinPeer);
 
